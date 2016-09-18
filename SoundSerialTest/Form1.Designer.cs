2785c8f010e87404b4dcb8543a9a7c9b46453d59
VictorHerbert <victorherberts@outlook.com>
2016-09-18 17:57:47 -0300

    Add files via upload

---
+++ SoundSerialTest/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿using System.Media;
+using System.Drawing;
+
+namespace SoundSerialTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(171, 17);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 44);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Conectar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(50, 252);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 40);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Falar Temperatura";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Enabled = false;
+            this.label1.Location = new System.Drawing.Point(7, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Conectar em ";
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.panel2.Location = new System.Drawing.Point(39, 125);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(261, 79);
+            this.panel2.TabIndex = 6;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Enabled = false;
+            this.comboBox1.ImeMode = System.Windows.Forms.ImeMode.Alpha;
+            this.comboBox1.Location = new System.Drawing.Point(135, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(-1, -1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Procurar Conexões";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Location = new System.Drawing.Point(39, 43);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(261, 51);
+            this.panel1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Enabled = false;
+            this.label2.Location = new System.Drawing.Point(188, 227);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Medições (vazio)";
+            // 
+            // listView1
+            // 
+            this.listView1.Enabled = false;
+            this.listView1.Location = new System.Drawing.Point(159, 257);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(137, 97);
+            this.listView1.TabIndex = 8;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(329, 405);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button3);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Medidor Sonoro";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ SoundSerialTest/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.IO.Ports;
+using System.Threading;
+ 
+
+namespace SoundSerialTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (SerialPort.GetPortNames().Length != 0)
+            {
+                button1.Text = "Atualizar Lista";
+                comboBox1.Enabled = true;
+                button2.Enabled = true;
+                comboBox1.Items.Clear();
+                foreach (string port in SerialPort.GetPortNames())
+                {
+                    comboBox1.Items.Add(port);
+                }
+                comboBox1.SelectedIndex = 0;
+                label1.Text = "Conectar em " + comboBox1.Items[comboBox1.SelectedIndex].ToString();
+                label1.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("Por favor, conecte o arduino","Nenhuma Conexão encontrada", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+       
+        private void button2_Click(object sender, EventArgs e)
+        {
+            serialPort1.PortName = comboBox1.Items[comboBox1.SelectedIndex].ToString();
+            serialPort1.Open();
+            if (serialPort1.IsOpen) label1.Text = "Conectado em " + serialPort1.PortName;
+            button1.Enabled = false;
+            comboBox1.Enabled = false;
+            button2.Enabled = false;
+            label1.Enabled = true;
+            button3.Enabled = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SoundPlayer Leitor;
+            serialPort1.Write("3");
+            string valor = serialPort1.ReadLine();
+            if (valor[0] == '|')
+            {
+                string path;
+                label2.Text = "Medições";
+
+                if (valor.Length.Equals(9))
+                {
+                    path = "Audio/" + valor[1] + "00.wav";
+                    Leitor = new SoundPlayer(path);
+                    Leitor.PlaySync();
+                    if (valor[2] == 1)
+                    {
+                        path = "Audio/" + valor[2].ToString() + valor[3].ToString() + "0.wav";
+                        Leitor = new SoundPlayer(path);
+                        Leitor.PlaySync();
+                    }
+                    else
+                    {
+                        path = "Audio/" + valor[2] + "0.wav";
+                        Leitor = new SoundPlayer(path);
+                        Leitor.PlaySync();
+
+                        Leitor = new SoundPlayer(@"Audio/e.wav");
+                        Leitor.PlaySync();
+
+                        path = "Audio/" + valor[3] + ".wav";
+                        Leitor = new SoundPlayer(path);
+                        Leitor.PlaySync();
+                    }
+                    Leitor = new SoundPlayer(@"Audio/ponto.wav");
+                    Leitor.PlaySync();
+
+                    path = "Audio/" + valor[5] + ".wav";
+                    Leitor = new SoundPlayer(path);
+                    Leitor.PlaySync();
+
+                    path = "Audio/" + valor[6] + ".wav";
+                    Leitor = new SoundPlayer(path);
+                    Leitor.PlaySync();
+
+                }
+                if (valor.Length.Equals(8))
+                {
+                    if (valor[1] == 1)
+                    {
+                        path = "Audio/" + valor[1].ToString() + valor[2].ToString()+ "0.wav";
+                        Leitor = new SoundPlayer(path);
+                        Leitor.PlaySync();
+                    }
+                    else
+                    {
+                        path = "Audio/" + valor[1] + "0.wav";
+                        Leitor = new SoundPlayer(path);
+                        Leitor.PlaySync();
+
+                        Leitor = new SoundPlayer(@"Audio/e.wav");
+                        Leitor.PlaySync();
+
+                        path = "Audio/" + valor[2] + ".wav";
+                        Leitor = new SoundPlayer(path);
+                        Leitor.PlaySync();
+                    }
+                    
+                    Leitor = new SoundPlayer(@"Audio/ponto.wav");
+                    Leitor.PlaySync();
+
+                    path = "Audio/" + valor[4] + ".wav";
+                    Leitor = new SoundPlayer(path);
+                    Leitor.PlaySync();
+
+                    path = "Audio/" + valor[5] + ".wav";
+                    Leitor = new SoundPlayer(path);
+                    Leitor.PlaySync();
+                }
+                if (valor.Length.Equals(7))
+                {
+                    path = "Audio/" + valor[1] + ".wav";
+                    Leitor = new SoundPlayer(path);
+                    Leitor.PlaySync();
+
+                    Leitor = new SoundPlayer(@"Audio/ponto.wav");
+                    Leitor.PlaySync();
+
+                    path = "Audio/" + valor[2] + ".wav";
+                    Leitor = new SoundPlayer(path);
+                    Leitor.PlaySync();
+
+                    path = "Audio/" + valor[3] + ".wav";
+                    Leitor = new SoundPlayer(path);
+                    Leitor.PlaySync();
+                }
+                SoundPlayer Leitor2 = new SoundPlayer(@"Audio/°C.wav");
+                Leitor2.PlaySync();
+            }
+            label2.Enabled = true;
+            listView1.Enabled = true;
+            valor += "°C";
+            listView1.Items.Add(valor);
+        }
+    }
+}
+
